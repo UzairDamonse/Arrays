b9ecbbb8dc8738ae48b59f95e9ab999f2e107238
UzairDamonse <uzairdamonse03@gmail.com>
2022-06-06 15:26:53 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,102 @@
+// Projects
+const projects = [
+  {
+    title: "Restaurant",
+    description:
+      "This project was my firt project that included everything I learned with Bootstrap",
+    techStack: "HTML/CSS",
+    github: "https://github.com/UzairDamonse/Restaurant",
+    live: "https://restuarant-practice.netlify.app/",
+    imgURL: " https://i.postimg.cc/5Q4y1mTP/Restaurant.png",
+  },
+  {
+    title: "Dream-destination",
+    description: "This project was my firt project that included HTML and CSS",
+    techStack: "HTML/CSS",
+    github: "https://github.com/UzairDamonse/DreamDestinationV2",
+    live: "https://dream-destination-v2.netlify.app/",
+    imgURL: "https://i.postimg.cc/bdxwJ4Rs/Dream-destination.png ",
+  },
+  {
+    title: "Calculator",
+    description: "This project was my firt project that included flexbox",
+    techStack: "HTML/CSS",
+    github: "https://github.com/UzairDamonse/Calculator",
+    live: "https://calculator-eg.netlify.app/",
+    imgURL: "https://i.postimg.cc/ygSWvd9P/Calculator.png",
+  },
+];
+
+// Testimonials
+
+const testimonials = [
+  {
+    name: "Jason Wandrag",
+    testimonial:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velaperiam, harum qui soluta, nesciunt at facilis excepturiconsequuntur aut provident tenetur distinctio consequatur iustoquam fugiat repellendus vitae eum neque alias, deleniti dolorem.Deserunt, voluptas! ",
+    occupation: "Lecturer",
+    profile: "https://i.postimg.cc/PPfvktkt/Jason.jpg",
+  },
+  {
+    name: "Sandy Williams",
+    testimonial:
+      "Uzair Damonse joined St John Brigade in the year 2014. He then became a member of the brigade and volunteered his time to helping others in need. Uzair is a valued member of the St John Brigade and has contributed a lot of his time as a volunteer. Uzair is a hardworking, determined, and intelligent young man",
+    occupation: "Lecturer",
+    profile: "https://i.postimg.cc/7b9b9pJj/Sandy.jpg",
+  },
+  {
+    name: "Ricardo Ronan Moses",
+    testimonial:
+      "Uzair Damonse is a very animated young man. He is never afraid to express his thoughts . He has the ability to communicate clearly when working with a group and has an excellent ability to ensure the project reaches it's desired outcome.",
+    occupation: "Peer",
+    profile: "https://i.postimg.cc/jLGT4XcG/Ricardo.jpg",
+  },
+  {
+    name: "Fatima Gallant",
+    testimonial:
+      "Uzair is a fast learner with the ability to pick up new skills swiftly. With a natural ability for all things technical, Uzair will find a way to fix any problem and overcome any challenge. His competitive nature helps him to exceed in his field. It's been an absolute pleasure working alongside such a bright-minded guy.",
+    occupation: "Peer",
+    profile: "https://i.postimg.cc/PLntT48m/Fatima.jpg",
+  },
+  {
+    name: "Jesse Spence",
+    testimonial:
+      "Uzair is an extremely intelligent individual, he is clearly able to grasp complex problems faster than the average person. He has a keen eye for detail and diligently works toward perfection. He's also kind and open-minded which makes him an absolute delight to be around.",
+    occupation: "Peer",
+    profile: "https://i.postimg.cc/phDRmnzg/Jesse.jpg",
+  },
+  {
+    name: "Carla Amy Lawrence",
+    testimonial:
+      "Uzair Demonse is an excellent web developer. His friendly disposition and vast skills makes him an asset to any team. Uzair's work is consistently  displayed in both a creative and professional manner. His vision translates beautifully to ensure a well crafted user interface and functional user experience.",
+    occupation: "Peer",
+    profile: "https://i.postimg.cc/xqncK08K/Carla.jpg",
+  },
+];
+
+// Resume
+
+const resume = [
+  {
+    date: "2021-2022",
+    place: "ATK Arena",
+    role: "Intern",
+    responsibility:
+      "I worked at ATK Arena during the December holidays of 2021 as an intern where I was responsible for customer service as well as helping with any technical problems customers faced.",
+  },
+  {
+    date: "2017-2021",
+    place: "Cape Academy of Maths, Science and Technology",
+    role: "Matric",
+    responsibility: "I matriculated in 2021 where I graduated with a dipolma",
+  },
+  {
+    date: "2022-Present",
+    place: "Life Choices",
+    role: "Student",
+    responsibility:
+      "I am attending a full-time coding course where I am being taught to become a full-stack web developer",
+  },
+];
+
+console.log(projects, testimonials, resume);
